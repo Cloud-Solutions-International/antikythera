139e2ac39984b66b786d9ebb09fb297699eef4ec
Lahiru.Bandara <Lahiru.Bandara@cloudsolutions.com.sa>
2024-09-09 18:17:11 +0530

    Removed unnecessary headers [V4-7508]

--- src/main/java/com/cloud/api/constants/StaticHeaders.java
+++ src/main/java/com/cloud/api/constants/StaticHeaders.java
@@ -10,8 +10,5 @@ public interface StaticHeaders {
     Header X_USER = new Header("x-user", "5550");
     Header ACCEPT_ALL = new Header("Accept", "*/*");
     Header X_LOCATION = new Header("x-location", "31042");
-    Header X_PHRLOCATION = new Header("x-location", "23050");
-    Header X_PHRUSER = new Header("x-user", "3091");
-    Header X_MODULE = new Header("x-module", "phr");
 
 }
