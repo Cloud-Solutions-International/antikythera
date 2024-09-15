6f3f0d9bbe050975bc6aae6bcc4131c9b8be3a46
raditha dissanayake <raditha@raditha.com>
2024-09-15 05:46:04 +0530

    Jackson xml seems to have limitations V4-7933

--- src/main/java/com/cloud/api/generator/ProjectGenerator.java
+++ src/main/java/com/cloud/api/generator/ProjectGenerator.java
@@ -1,6 +1,10 @@
 package com.cloud.api.generator;
 
 import com.cloud.api.configurations.Settings;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.io.*;
 import java.nio.channels.Channels;
@@ -18,6 +22,9 @@ public class ProjectGenerator {
     private static final String SUFFIX = ".java";
 
     private static ProjectGenerator instance;
+
+    @JacksonXmlRootElement(localName = "project", namespace = "http://maven.apache.org/POM/4.0.0")
+    public static class ProjectNode {}
 
     private ProjectGenerator() {
         basePath = Settings.getProperty("BASE_PATH");
@@ -62,6 +69,38 @@ public class ProjectGenerator {
         }
     }
 
+    public void copyPom() throws IOException {
+        if (Settings.getProperty("DEPENDENCIES") == null) {
+            copyTemplate("pom.xml");
+        } else {
+            String[] dependencies = Settings.getProperty("DEPENDENCIES").split(",");
+            Path destinationPath = Path.of(outputPath, "pom.xml");     // Path where template file should be copied into
+            if(!Files.exists(destinationPath.getParent())) {
+                Files.createDirectories(destinationPath.getParent());
+            }
+
+            XmlMapper xmlMapper = new XmlMapper();
+            xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+            JsonNode template = xmlMapper.readTree(getClass().getClassLoader().getResourceAsStream("templates/pom.xml"));
+            Path p = basePath.contains("src/main/java")
+                    ? Paths.get(basePath.replace("/src/main/java", ""), "pom.xml")
+                    : Paths.get(basePath, "pom.xml");
+
+            JsonNode src = xmlMapper.readTree(p.toFile());
+
+            for (String dep : dependencies) {
+                src.path("dependencies").path("dependency").forEach(dependency -> {
+                    if (dependency.path("artifactId").asText().equals(dep)) {
+                        ((com.fasterxml.jackson.databind.node.ArrayNode) template.path("dependencies").path("dependency")).add(dependency);
+                    }
+                });
+            }
+            xmlMapper.addMixIn(JsonNode.class, ProjectNode.class);
+            xmlMapper.writerWithDefaultPrettyPrinter().writeValue(new File(destinationPath.toString()), template);
+        }
+    }
+
     public static void copyFolder(Path source, Path destination) throws IOException {
         if (!Files.exists(destination)) {
             Files.createDirectories(destination);
@@ -84,7 +123,7 @@ public class ProjectGenerator {
     }
 
     private void copyBaseFiles(String outputPath) throws IOException {
-        copyTemplate("pom.xml");
+        copyPom();
         copyTemplate("TestHelper.java", "src", "test", "java", "com", "cloud", "api", "base");
 
         Path pathToCopy = Paths.get(outputPath, "src", "test", "resources");
