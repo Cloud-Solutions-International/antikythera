df1d4644520955d00957a7faf7259d3d10343fbe
raditha dissanayake <raditha@raditha.com>
2024-09-16 21:33:49 +0530

    Looking into the Repositories V4-7979

---
+++ src/main/java/com/cloud/api/generator/RepositoryParser.java
@@ -0,0 +1,65 @@
+package com.cloud.api.generator;
+
+import com.cloud.api.configurations.Settings;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class RepositoryParser extends ClassProcessor{
+
+    public RepositoryParser() throws IOException {
+        super();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Settings.loadConfigMap();
+        RepositoryParser parser = new RepositoryParser();
+        parser.process();
+    }
+
+    private void process() throws FileNotFoundException {
+        File f = new File("/home/raditha/workspace/python/CSI/selenium/repos/EHR-IP/csi-ehr-ip-java-sev/src/main/java/com/csi/vidaplus/ehr/ip/admissionwithcareplane/dao/discharge/DischargeDetailRepository.java");
+        CompilationUnit cu = javaParser.parse(f).getResult().orElseThrow(() -> new IllegalStateException("Parse error"));
+        cu.accept(new Visitor(), null);
+    }
+
+    class Visitor extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(MethodDeclaration n, Void arg) {
+            super.visit(n, arg);
+            System.out.println("Method: " + n.getName());
+            for(var ann : n.getAnnotations()) {
+                if(ann.getNameAsString().equals("Query")) {
+                    String query = null;
+                    if (ann.isSingleMemberAnnotationExpr()) {
+                        query = ann.asSingleMemberAnnotationExpr().getMemberValue().toString().replace("\"","");
+                    } else if (ann.isNormalAnnotationExpr()) {
+                        boolean nt = false;
+                        for(var pair : ann.asNormalAnnotationExpr().getPairs()) {
+                            if(pair.getNameAsString().equals("nativeQuery")) {
+                                if(pair.getValue().toString().equals("true")) {
+                                    nt = true;
+                                    System.out.println("\tNative Query");
+                                }
+                                else {
+                                    System.out.println("\tJPQL Query");
+                                }
+                            }
+                        }
+                        for(var pair : ann.asNormalAnnotationExpr().getPairs()) {
+                            if(pair.getNameAsString().equals("value")) {
+                                query = pair.getValue().toString().replace("\"","");
+                            }
+                        }
+                    }
+                    System.out.println(query);
+                }
+            }
+        }
+    }
+}
