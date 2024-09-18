fec5d043363241b8e0519c15b2b7ca7dab2c2e5d
Nimesh <ariyarathnanimesh@gmail.com>
2024-09-18 08:20:17 +0530

    Fix issue of setting values for LocalDate fields [V4-8011]

--- src/main/java/com/cloud/api/generator/DTOHandler.java
+++ src/main/java/com/cloud/api/generator/DTOHandler.java
@@ -335,6 +335,10 @@ public class DTOHandler extends  ClassProcessor{
                         setter.addArgument("UUID.randomUUID()");
                         break;
 
+                    case "LocalDate":
+                        setter.addArgument("LocalDate.now()");
+                        break;
+
                     default:
                         if(!field.resolve().getType().asReferenceType().getTypeDeclaration().get().isEnum()) {
                             setter.addArgument("new " + type + "()");
