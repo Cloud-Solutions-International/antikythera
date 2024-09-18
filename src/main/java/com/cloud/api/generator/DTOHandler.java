4fdbeb313e3db4b2aaa2bfd255bf96fc8a0414b9
Balara Jayaweera <balarajayaweera@gmail.com>
2024-09-18 16:17:02 +0530

    Add missing types to DTOHandler.TypeCollector generateRandomValue

--- src/main/java/com/cloud/api/generator/DTOHandler.java
+++ src/main/java/com/cloud/api/generator/DTOHandler.java
@@ -293,8 +293,7 @@ public class DTOHandler extends  ClassProcessor{
                     case "Date":
                         if(field.getElementType().toString().contains(".")) {
                             setter.addArgument("new java.util.Date()");
-                        }
-                        else {
+                        } else {
                             setter.addArgument("new Date()");
                         }
                         break;
@@ -303,6 +302,7 @@ public class DTOHandler extends  ClassProcessor{
                     case "double":
                         setter.addArgument("0.0");
                         break;
+
                     case "Float":
                     case "float":
                         setter.addArgument("0.0f");
@@ -312,6 +312,12 @@ public class DTOHandler extends  ClassProcessor{
                     case "int":
                         setter.addArgument("0");
                         break;
+
+                    case "byte":
+                    case "Byte":
+                        setter.addArgument("(byte) 0");
+                        break;
+
                     case "List":
                         setter.addArgument("List.of()");
                         break;
@@ -320,6 +326,7 @@ public class DTOHandler extends  ClassProcessor{
                     case "Long":
                         setter.addArgument("0L");
                         break;
+
                     case "String":
                         setter.addArgument("\"Hello world\"");
                         break;
@@ -327,6 +334,7 @@ public class DTOHandler extends  ClassProcessor{
                     case "Map":
                         setter.addArgument("Map.of()");
                         break;
+
                     case "Set":
                         setter.addArgument("Set.of()");
                         break;
@@ -338,11 +346,11 @@ public class DTOHandler extends  ClassProcessor{
                     default:
                         if(!field.resolve().getType().asReferenceType().getTypeDeclaration().get().isEnum()) {
                             setter.addArgument("new " + type + "()");
-                        }
-                        else {
+                        } else {
                             setter = null;
                         }
                 }
+
                 if(setter != null) {
                     method.getBody().get().addStatement(setter);
                 }
