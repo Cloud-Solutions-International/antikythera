df9505a8f34af8cd43ab226322ca885dea147fda
raditha dissanayake <raditha@raditha.com>
2024-09-22 21:51:35 +0530

    Trying to map the request/path variables to query parameters V4-8348

--- src/main/java/com/cloud/api/generator/RepositoryQuery.java
+++ src/main/java/com/cloud/api/generator/RepositoryQuery.java
@@ -1,9 +1,9 @@
 package com.cloud.api.generator;
 
-import net.sf.jsqlparser.expression.Expression;
-
 import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a query from a JPARepository
@@ -14,23 +14,33 @@ public class RepositoryQuery {
      * This is the value of the native flag to the @Query annotation.
      */
     boolean isNative;
-    String query;
 
+    /**
+     * The string representation of the query.
+     */
+    String query;
 
     /**
      * The result set from the last execution of this query if any
      */
     private ResultSet resultSet;
 
-
     /**
      * The list of columns that were removed from the query where clause or grouping.
      */
     private List<String> removed;
 
+    /**
+     * Represents the mapping of the request or path parameters to the query parameters.
+     *
+     * The key is the name of the parameter in the query.
+     */
+    private Map<String, String> parameterMap;
+
     public RepositoryQuery(String query, boolean isNative) {
         this.isNative = isNative;
         this.query = query;
+        parameterMap = new HashMap<>();
     }
 
     public boolean isNative() {
@@ -56,5 +66,9 @@ public class RepositoryQuery {
 
     public void setResultSet(ResultSet resultSet) {
         this.resultSet = resultSet;
+    }
+
+    public Map<String, String> getParameterMap() {
+        return parameterMap;
     }
 }
