51012adc00d642cc3843a143906d665a7cc6c66a
raditha dissanayake <raditha@raditha.com>
2024-09-28 21:54:14 +0530

    The evaluator is now aware of lists and maps V4-8436

---
+++ src/test/java/com/cloud/api/evaluator/Bunches.java
@@ -0,0 +1,30 @@
+package com.cloud.api.evaluator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Bunches {
+
+    public static void main(String[] args) {
+        Bunches bunches = new Bunches();
+        bunches.printList();
+        bunches.printMap();
+    }
+
+
+    public void printList() {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        System.out.println(list);
+    }
+
+    public void printMap() {
+        Map<String, Integer> list = new HashMap<>();
+        list.put("one", 1);
+        list.put("two", 2);
+        System.out.println(list);
+    }
+}
