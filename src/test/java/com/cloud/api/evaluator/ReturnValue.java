cf39ce604e6aa0ef2828f5fb9822745bc689025e
raditha dissanayake <raditha@raditha.com>
2024-09-28 16:53:16 +0530

    Fixed bug in return value V4-8810

---
+++ src/test/java/com/cloud/api/evaluator/ReturnValue.java
@@ -0,0 +1,14 @@
+package com.cloud.api.evaluator;
+
+public class ReturnValue {
+
+    public void printName() {
+        System.out.println(getName());
+    }
+
+    public String getName() {
+        return "John";
+    }
+}
+
+
