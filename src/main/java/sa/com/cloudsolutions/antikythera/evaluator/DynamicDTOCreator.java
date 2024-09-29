e6257539d08fed77efdca8d0bc7627093cb44a51
raditha dissanayake <raditha@raditha.com>
2024-09-29 19:20:16 +0530

    Hello ByteBuddy V4-8830

---
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/DynamicDTOCreator.java
@@ -0,0 +1,34 @@
+package sa.com.cloudsolutions.antikythera.evaluator;
+
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+
+import com.github.javaparser.resolution.declarations.ResolvedFieldDeclaration;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+
+
+public class DynamicDTOCreator {
+
+    public static Class<?> createDynamicDTO(ClassOrInterfaceType dtoType) throws Exception {
+        String className = dtoType.getNameAsString();
+        ByteBuddy byteBuddy = new ByteBuddy();
+
+        DynamicType.Builder<?> builder = byteBuddy.subclass(Object.class).name(className);
+
+
+        for (ResolvedFieldDeclaration field : dtoType.resolve().asReferenceType().getDeclaredFields()) {
+            String fieldName = field.getName();
+            String method = "get" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
+            builder = builder.defineMethod(method, Object.class, net.bytebuddy.description.modifier.Visibility.PUBLIC)
+                    .intercept(FixedValue.nullValue());
+        }
+
+        return builder.make()
+                .load(DynamicDTOCreator.class.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+                .getLoaded();
+    }
+
+}
