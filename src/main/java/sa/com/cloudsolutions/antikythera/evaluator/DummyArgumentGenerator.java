5037b5e6d9eef997bc401131f400a688c358d3d0
Raditha Dissanayake <raditha.dissanayake@cloudsolutions.com.sa>
2024-10-30 20:02:30 +0530

    Argument Generator

---
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/DummyArgumentGenerator.java
@@ -0,0 +1,15 @@
+package sa.com.cloudsolutions.antikythera.evaluator;
+
+public class DummyArgumentGenerator extends ArgumentGenerator {
+
+     public Variable mockParameter(String typeName) {
+        return new Variable(switch (typeName) {
+            case "Boolean" -> false;
+            case "float", "Float", "double", "Double" -> 0.0;
+            case "Integer", "int" -> 0;
+            case "Long", "long" -> 0L;
+            case "String" -> "Ibuprofen";
+            default -> "0";
+        });
+    }
+}
--- src/main/java/sa/com/cloudsolutions/antikythera/evaluator/SpringEvaluator.java
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/SpringEvaluator.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import sa.com.cloudsolutions.antikythera.exception.AntikytheraException;
 import sa.com.cloudsolutions.antikythera.generator.QueryMethodArgument;
-import sa.com.cloudsolutions.antikythera.generator.QueryMethodParameter;
 import sa.com.cloudsolutions.antikythera.generator.TruthTable;
 import sa.com.cloudsolutions.antikythera.parser.AbstractCompiler;
 import sa.com.cloudsolutions.antikythera.parser.ClassProcessor;
@@ -95,6 +94,7 @@ public class SpringEvaluator extends Evaluator {
 
     private boolean onTest;
 
+    private ArgumentGenerator argumentGenerator;
     /**
      * It is better to use create evaluator
      * @param className the name of the class associated with this evaluator
@@ -208,80 +208,10 @@ public class SpringEvaluator extends Evaluator {
     private void mockURIVariables(MethodDeclaration md) throws ReflectiveOperationException {
         for (int i = md.getParameters().size() - 1; i >= 0; i--) {
             var param = md.getParameter(i);
-            String paramString = String.valueOf(param);
-
-            if (paramString.startsWith("@RequestBody")) {
-                /*
-                 * Request body on the other hand will be more complex and will most likely be a DTO.
-                 */
-                Type t = param.getType();
-                if (t.isClassOrInterfaceType()) {
-                    String fullClassName = t.asClassOrInterfaceType().resolve().asReferenceType().getQualifiedName();
-                    if (fullClassName.startsWith("java")) {
-                        /*
-                         * However you can't rule out the possibility that this is a Map or a List or even a
-                         * boxed type.
-                         */
-                        if (t.asClassOrInterfaceType().isBoxedType()) {
-                            Variable v = mockParameter(param.getTypeAsString());
-                            /*
-                             * We need to push this variable to the stack so that it can be used later.
-                             */
-                            AntikytheraRunTime.push(v);
-                        }
-                        else {
-                            if (fullClassName.startsWith("java.util")) {
-                                Variable v = Reflect.variableFactory(fullClassName);
-                                /*
-                                 * Pushed to be popped later in the callee
-                                 */
-                                AntikytheraRunTime.push(v);
-                            }
-                            else {
-                                Class<?> clazz = Class.forName(fullClassName);
-                                Variable v = new Variable(clazz.newInstance());
-                                /*
-                                 * PUsh arguments
-                                 */
-                                AntikytheraRunTime.push(v);
-                            }
-                        }
-                    }
-                    else {
-
-                        Evaluator o = new SpringEvaluator(fullClassName);
-                        o.setupFields(AntikytheraRunTime.getCompilationUnit(fullClassName));
-                        Variable v = new Variable(o);
-                        /*
-                         * Args to be popped by the callee
-                         */
-                        AntikytheraRunTime.push(v);
-                    }
-                } else {
-                    logger.warn("Unhandled {}", t);
-                }
-            }
-            else {
-                /*
-                 * Request parameters are typically strings or numbers and these are pushed into the stack
-                 * to be popped in the callee
-                 */
-                Variable v = mockParameter(param.getTypeAsString());
-                AntikytheraRunTime.push(v);
-            }
+            argumentGenerator.generateArgument(param);
         }
     }
 
-    private static Variable mockParameter(String typeName) {
-        return new Variable(switch (typeName) {
-            case "Boolean" -> false;
-            case "float", "Float", "double", "Double" -> 0.0;
-            case "Integer", "int" -> 0;
-            case "Long", "long" -> 0L;
-            case "String" -> "Ibuprofen";
-            default -> "0";
-        });
-    }
 
     /**
      * Execute a query on a repository.
