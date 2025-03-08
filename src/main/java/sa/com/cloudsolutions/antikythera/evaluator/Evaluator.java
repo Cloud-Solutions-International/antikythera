785202ddc04387e0bd01495249baf88ed8c92c8b
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-03-08 09:42:13 +0530

    Code must continously be cleaned up

--- src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
@@ -1225,7 +1225,7 @@ public class Evaluator {
             case MINUS:
             case MULTIPLY:
             case DIVIDE:
-                return arithmeticOperation(left, right, operator);
+                return Arithmetics.operate(left, right, operator);
 
             default:
                 return null;
@@ -1255,49 +1255,6 @@ public class Evaluator {
             return new Variable(Boolean.TRUE);
         }
         return new Variable(left.getValue().equals(right.getValue()));
-    }
-
-
-    private static Number performOperation(Number left, Number right, BinaryExpr.Operator operator) {
-        return switch (operator) {
-            case PLUS -> left.doubleValue() + right.doubleValue();
-            case MINUS -> left.doubleValue() - right.doubleValue();
-            case DIVIDE -> left.doubleValue() / right.doubleValue();
-            case MULTIPLY -> left.doubleValue() * right.doubleValue();
-            default ->
-                throw new IllegalArgumentException("Unsupported operator: " + operator);
-        };
-    }
-
-    /**
-     * Simple arithmetic operations.
-     * String can be added to anything but numbers are tricker.
-     * @param left the left operand
-     * @param right the right operand
-     * @return the result of the add operation which may be arithmatic or string concatenation
-     */
-    static Variable arithmeticOperation(Variable left, Variable right, BinaryExpr.Operator operator) {
-        if (left.getValue() instanceof String || right.getValue() instanceof String) {
-            return new Variable(left.getValue().toString() + right.getValue().toString());
-        }
-        if (left.getValue() instanceof Number l && right.getValue() instanceof Number r) {
-            Number result = performOperation(l, r, operator);
-
-            if (l instanceof Double || r instanceof Double) {
-                return new Variable(result.doubleValue());
-            } else if (l instanceof Float || r instanceof Float) {
-                return new Variable(result.floatValue());
-            } else if (l instanceof Long || r instanceof Long) {
-                return new Variable(result.longValue());
-            } else if (l instanceof Integer || r instanceof Integer) {
-                return new Variable(result.intValue());
-            } else if (l instanceof Short || r instanceof Short) {
-                return new Variable(result.shortValue());
-            } else if (l instanceof Byte || r instanceof Byte) {
-                return new Variable(result.byteValue());
-            }
-        }
-        return null;
     }
 
     void identifyFieldDeclarations(VariableDeclarator variable) throws ReflectiveOperationException, IOException {
