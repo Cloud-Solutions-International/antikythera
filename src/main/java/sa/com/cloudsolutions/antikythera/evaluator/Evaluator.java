317ebe567f8920a0fd4013fbed0ebdad6ae97400
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-04-06 09:05:43 +0530

    Create using byte buddy was retired something else will take its place

--- src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
@@ -552,38 +552,12 @@ public class Evaluator {
 
         if (vx == null) {
             vx = createUsingReflection(type, oce);
-            if(vx == null) {
-                vx = createUsingByteBuddy(oce, type);
-            }
         }
         if (decl != null) {
             setLocal(instructionPointer, decl.getNameAsString(), vx);
         }
 
         return vx;
-    }
-
-    private Variable createUsingByteBuddy(ObjectCreationExpr oce, ClassOrInterfaceType type) {
-        if (Settings.getProperty("bytebuddy") != null) {
-
-            try {
-                List<Expression> arguments = oce.getArguments();
-                Object[] constructorArgs = new Object[arguments.size()];
-
-                for (int i = 0; i < arguments.size(); i++) {
-                    Variable arg = evaluateExpression(arguments.get(i));
-                    constructorArgs[i] = arg.getValue();
-                }
-
-                // Create the dynamic DTO with the extracted arguments
-                ImportWrapper imp = AbstractCompiler.findImport(cu, type.getNameAsString());
-                Object instance = DTOBuddy.createDynamicDTO(imp.getType().asClassOrInterfaceDeclaration());
-                return new Variable(type, instance);
-            } catch (Exception e) {
-                logger.error("An error occurred in creating a variable with bytebuddy", e);
-            }
-        }
-        return null;
     }
 
     /**
