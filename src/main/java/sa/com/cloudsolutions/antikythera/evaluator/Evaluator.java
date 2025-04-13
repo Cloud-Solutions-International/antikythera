105a3e89b2897483365f413baf96f29ae36475e2
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-04-13 22:30:36 +0530

    Oops that code went into the wrong class

--- src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
@@ -1455,28 +1455,6 @@ public class Evaluator {
         try {
             for (Statement stmt : statements) {
                 if(loops.isEmpty() || loops.peekLast().equals(Boolean.TRUE)) {
-                    if (stmt instanceof ExpressionStmt expressionStmt
-                            && expressionStmt.getExpression() instanceof MethodCallExpr methodCallExpr) {
-                        Optional<Expression> scope = methodCallExpr.getScope();
-                        if (scope.isPresent()) {
-                            Expression expr = scope.get();
-                            if(expr.isMethodCallExpr()) {
-                                MethodCallExpr mce = scope.get().asMethodCallExpr();
-                                if (mce.getNameAsString().equals("when")) {
-                                    Variable w = evaluateExpression(mce.getArguments().getFirst().orElseThrow());
-                                    Variable t = evaluateExpression(methodCallExpr.getArguments().getFirst().orElseThrow());
-
-                                    Mockito.when(w.getValue()).thenReturn(t.getValue());
-                                }
-                            }
-                            else if (expr.isFieldAccessExpr()) {
-                                FieldAccessExpr fce = scope.get().asFieldAccessExpr();
-                                if (fce.getNameAsString().equals("Mockito")) {
-
-                                }
-                            }
-                        }
-                    }
                     executeStatement(stmt);
                     if (returnFrom != null) {
                         break;
