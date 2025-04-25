5ccf068443fc32be3ae81038f72a30c0d575a91e
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-04-25 07:27:51 +0530

    NOw getting down to the really important matter of mocking queries

---
+++ src/test/java/sa/com/cloudsolutions/antikythera/evaluator/FakeRepository.java
@@ -0,0 +1,7 @@
+package sa.com.cloudsolutions.antikythera.evaluator;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FakeRepository extends JpaRepository<FakeEntity, Integer> {
+
+}
