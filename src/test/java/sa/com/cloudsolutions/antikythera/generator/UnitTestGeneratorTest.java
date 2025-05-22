f04676359d64802673a03f5e1a1a00fa88aff464
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-05-22 07:56:32 +0530

    That breaks 8

--- src/test/java/sa/com/cloudsolutions/antikythera/generator/UnitTestGeneratorTest.java
+++ src/test/java/sa/com/cloudsolutions/antikythera/generator/UnitTestGeneratorTest.java
@@ -89,11 +89,6 @@ class UnitTestGeneratorTest {
      */
     @Test
     void testSetUpBase() throws NoSuchMethodException {
-        Settings.setProperty(Settings.BASE_PATH,
-                Settings.getProperty(Settings.BASE_PATH, String.class)
-                        .orElse("").replace("src/test/resources/sources","")
-                        .replace("antikythera", "antikythera-test-helper")
-                        .replace("src/test","src/main"));
         unitTestGenerator.loadPredefinedBaseClassForTest("sa.com.cloudsolutions.antikythera.evaluator.mock.Hello");
 
         Method m = Statement.class.getDeclaredMethod("execute", String.class);
