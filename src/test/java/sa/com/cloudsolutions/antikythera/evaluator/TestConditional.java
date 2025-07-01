07ad8fcfce889d584e13820dd00cdc8796523816
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-07-01 07:43:11 +0530

    Tests for StringUtils.isEmpty

--- src/test/java/sa/com/cloudsolutions/antikythera/evaluator/TestConditional.java
+++ src/test/java/sa/com/cloudsolutions/antikythera/evaluator/TestConditional.java
@@ -68,7 +68,8 @@ class TestConditional extends TestHelper {
             "emptiness2, List is not empty!List is empty!",
             "emptiness3, List is empty!List is not empty!",
             "emptiness4, Set is empty!Set is not empty!",
-            "emptiness5, Map is empty!Map is not empty!"
+            "emptiness5, Map is empty!Map is not empty!",
+            "stringUtils, empty!Not empty!",
     })
     void testVisit(String name, String value) throws ReflectiveOperationException {
         ((SpringEvaluator)evaluator).setArgumentGenerator(new DummyArgumentGenerator());
