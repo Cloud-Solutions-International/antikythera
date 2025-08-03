e55f40f75ed3cc33af10e7a383f520ef9167c531
raditha dissanayake <raditha.dissanayake@gmail.com>
2025-08-03 17:42:52 +0530

    Junie removed some stuff that it should not have

--- src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
+++ src/main/java/sa/com/cloudsolutions/antikythera/evaluator/Evaluator.java
@@ -89,6 +89,8 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 
 /**
@@ -1131,13 +1133,33 @@ public class Evaluator {
                 returnValue.setClazz(method.getReturnType());
             }
         } catch (InaccessibleObjectException ioe) {
-            // If module access fails, try to find a public interface or superclass method
-            Method publicMethod = Reflect.findPublicMethod(v.getClazz(), reflectionArguments.getMethodName(), reflectionArguments.getArgumentTypes());
-            if (publicMethod != null) {
-                returnValue = new Variable(publicMethod.invoke(v.getValue(), finalArgs));
-                if (returnValue.getValue() == null && returnValue.getClazz() == null) {
-                    returnValue.setClazz(publicMethod.getReturnType());
+            // Handle JDK stream methods differently
+            if (v.getClazz().getName().startsWith("java.util.stream.")) {
+                handleStreamMethods(v, reflectionArguments);
+            } else {
+                Method publicMethod = Reflect.findPublicMethod(v.getClazz(),
+                        reflectionArguments.getMethodName(),
+                        reflectionArguments.getArgumentTypes());
+                if (publicMethod != null) {
+                    returnValue = new Variable(publicMethod.invoke(v.getValue(), finalArgs));
+                    if (returnValue.getValue() == null && returnValue.getClazz() == null) {
+                        returnValue.setClazz(publicMethod.getReturnType());
+                    }
                 }
+            }
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "java:S3740"})
+    private void handleStreamMethods(Variable v, ReflectionArguments reflectionArguments) {
+        String methodName = reflectionArguments.getMethodName();
+        Object obj = v.getValue();
+
+        if ("forEach".equals(methodName)) {
+            Consumer<?> action = (Consumer<?>) reflectionArguments.getFinalArgs()[0];
+            if (obj instanceof Stream stream) {
+                stream.forEach(action);
+                returnValue = new Variable(null); // void method
             }
         }
     }
